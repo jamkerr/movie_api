09ea4ef9d4bd15f0e3c0133f36f3544c027b1ce7
jamkerr <kerr0088@gmail.com>
2022-10-17 18:14:33 +0200

    Fear 2.4: Refactor app with Express Static files (like docs) are now served from "public" Top 10 movies are returned as json Logging is done by morgan Error handling is done by a middleware function

--- index.js
+++ index.js
@@ -1,2 +1,67 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
 const app = express();
+
+app.use(morgan('common'));
+
+let topTenMovies = [
+    {
+        title: 'The Adventures of Priscilla, Queen of the Desert',
+        director: 'Stephan Elliott'
+    },
+    {
+        title: 'Everything Everywhere All at Once',
+        director: ['Daniel Kwan', 'Daniel Scheinert']
+    },
+    {
+        title: 'Fight Club',
+        director: 'David Fincher'
+    },
+    {
+        title: 'The Matrix',
+        director: 'The Wachowskis'
+    },
+    {
+        title: 'Shaun of the Dead',
+        director: 'Edgar Wright'
+    },
+    {
+        title: 'The Rocky Horror Picture Show',
+        director: 'Jim Sharman'
+    },
+    {
+        title: 'The Truman Show',
+        director: 'Peter Weir'
+    },
+    {
+        title: 'Eternal Sunshine of the Spotless Mind',
+        director: 'Michel Gondry'
+    },
+    {
+        title: '2001: A Space Odyssey',
+        director: 'Stanley Kubrick'
+    },
+    {
+        title: 'Picnic at Hanging Rock',
+        director: 'Peter Weir'
+    }
+];
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Your Movie DataBase!');
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topTenMovies);
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Uh oh spaghetti-o...');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});

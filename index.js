e5653e1a5971062df75c7518d34209a13f7ad424
jamkerr <kerr0088@gmail.com>
2022-10-30 20:32:57 +0100

    Feat 2.8: Include Mongoose and migrate some API endpoints

--- index.js
+++ index.js
@@ -1,64 +1,18 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    Models = require('./models.js');
 
 const app = express();
+const Movies = Models.Movie;
+const Users = Models.User;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const allMovies = [
-    {
-        title: 'The Adventures of Priscilla, Queen of the Desert',
-        director: 'Stephan Elliott'
-    },
-    {
-        title: 'Everything Everywhere All at Once',
-        director: ['Daniel Kwan', 'Daniel Scheinert']
-    },
-    {
-        title: 'Fight Club',
-        director: 'David Fincher'
-    },
-    {
-        title: 'The Matrix',
-        director: 'The Wachowskis'
-    },
-    {
-        title: 'Shaun of the Dead',
-        director: 'Edgar Wright'
-    },
-    {
-        title: 'The Rocky Horror Picture Show',
-        director: 'Jim Sharman'
-    },
-    {
-        title: 'The Truman Show',
-        director: 'Peter Weir'
-    },
-    {
-        title: 'Eternal Sunshine of the Spotless Mind',
-        director: 'Michel Gondry'
-    },
-    {
-        title: '2001: A Space Odyssey',
-        director: 'Stanley Kubrick'
-    },
-    {
-        title: 'Picnic at Hanging Rock',
-        director: 'Peter Weir'
-    }
-];
+mongoose.connect('mongodb://localhost:27017/YMDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let users = [
-    {
-        username: 'Fred',
-        favorites: []
-    },
-    {
-        username: 'Agatha',
-        favorites: []
-    }
-]
 
 // Log requested URL
 app.use(morgan('common'));
@@ -74,7 +28,14 @@ app.get('/', (req, res) => {
 
 // Return all movies as json
 app.get('/movies', (req, res) => {
-  res.json(allMovies);
+    Movies.find()
+    .then((movies) => {
+        res.status(201).json(movies);
+    })
+    .catch((err) => {
+        console.err(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 // Return a single movie as json
@@ -93,53 +54,128 @@ app.get('/directors/:directorName', (req, res) => {
     res.send(`Here's info on the director ${req.params.directorName}.`);
 });
 
+// Return all users
+app.get('/users', (req, res) => {
+    Users.find()
+    .then((users) => {
+        res.status(201).json(users);
+    })
+    .catch((err) => {
+        console.err(err);
+        res.status(500).send('Error: ' + err);
+    });
+});
+
+// Return single user by username
+app.get('/users/:Username', (req, res) => {
+    Users.findOne({ Username: req.params.Username })
+      .then((user) => {
+        res.json(user);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
 // Register a new user
+/* JSON format:
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birth:Date: Date
+}*/
 app.post('/users', (req, res) => {
-    let newUser = req.body;
-  
-    if (!newUser.username) {
-        const message = 'Missing username in request body';
-        res.status(400).send(message);
-    } else {
-        newUser.favorites = [];
-        users.push(newUser);
-        res.status(201).send(newUser);
-    }
+    Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birth_Date: req.body.Birth_Date
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 // Update a username
 app.put('/users/:username', (req, res) => {
-    // Find user with current username
-    let user = users.find((user) => { return user.username === req.params.username });
-    let newUsername = req.body.newname;
-    if (user) {
-        user.username = newUsername;
-        res.status(201).send(`Your new username is ${newUsername}`);
-    } else {
-        res.status(404).send(`We couldn\'t find a user with the username ${req.params.username}`);
-    }
+    Users.findOneAndUpdate(
+        {Username: req.params.username},
+        {$set: {Username: req.body.Username}},
+        {new: true}
+    )
+    .then((updatedUser) => {res.status(201).json(updatedUser)})
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
+        
 
 // Add a favorite movie
-app.put('/users/:username/favorites', (req, res) => {
-    // Find user by username
-    let user = users.find((user) => { return user.username === req.params.username });
-    let newFavorite = req.body;
+// app.put('/users/:username/favorites', (req, res) => {
+//     // Find user by username
+//     let user = users.find((user) => { return user.username === req.params.username });
+//     let newFavorite = req.body;
 
 
-    // If user exists and they don't already have the film as a favorite
-    if (user) {
-        let isFavorite = user.favorites.find((favorite) => { return favorite.title === newFavorite.title});
-        if (!isFavorite) {
-            user.favorites.push(newFavorite);
-            // res.status(201).send(`You added ${newFavorite.title} as a favorite.`);
-            res.status(201).send(users);
-        } else {
-        res.status(404).send(`You already have ${newFavorite.title} as a favorite.`);
-        }
-    } else {
-        res.status(404).send(`We couldn\'t find a user with the username ${req.params.username}`);
-    }
+//     // If user exists and they don't already have the film as a favorite
+//     if (user) {
+//         let isFavorite = user.favorites.find((favorite) => { return favorite.title === newFavorite.title});
+//         if (!isFavorite) {
+//             user.favorites.push(newFavorite);
+//             // res.status(201).send(`You added ${newFavorite.title} as a favorite.`);
+//             res.status(201).send(users);
+//         } else {
+//             res.status(404).send(`You already have ${newFavorite.title} as a favorite.`);
+//         }
+//     } else {
+//         res.status(404).send(`We couldn\'t find a user with the username ${req.params.username}`);
+//     }
+// });
+
+// Add a favorite movie v2
+// app.put('/users/:username/favorites/:moviename', (req, res) => {
+//     Users.findOneAndUpdate(
+//         {Username: req.params.username},
+//         {$push: {FavoriteMovies: Movies.findOne({Title: req.params.moviename})._id}},
+//         {new: true}
+//     )
+//     .then((updatedUser) => {res.status(201).json(updatedUser)})
+//     .catch((err) => {
+//         console.error(err);
+//         res.status(500).send('Error: ' + err);
+//     });
+// });
+
+// Add a favorite movie v3
+app.put('/users/:username/favorites/:moviename', (req, res) => {
+    Users.findOneAndUpdate(
+        {Username: req.params.username},
+        {$push: {FavoriteMovies: Movies.findOne({Title: req.params.moviename}, '_id')}},
+        {new: true}
+    )
+    .then((updatedUser) => {res.status(201).json(updatedUser)})
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 // Remove a favorite movie

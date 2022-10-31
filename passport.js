113623b3fccf5d42f0285ecff8b8ff61e27c6b20
jamkerr <kerr0088@gmail.com>
2022-10-31 19:29:13 +0100

    Feat 2.9: Include passport and begin auth setup

---
+++ passport.js
@@ -0,0 +1,31 @@
+const passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
+    Models = require('./models.js'),
+    passportJWT = require('passport-jwt');
+
+let Users = Models.User,
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password'
+    },
+    (username, password, callback) => {
+        console.log(username + '  ' + password);
+        Users.findOne({username: username}, (error, user) => {
+            if (error) {
+                console.log(error);
+                return callback(error);
+            }
+            
+            if (!user) {
+                console.log('Incorrect username');
+                return callback(null, false, {message: 'Incorrect username or password.'});
+            }
+
+            console.log('Finished');
+            return callback(null, user);
+        });
+    }
+));

f56c444fc38771c2396e474ea69f40d4d87b911b
jamkerr <kerr0088@gmail.com>
2022-11-15 10:54:04 +0100

    Testing movies endpoint

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 });
 
 // Return all movies as json
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(200).json(movies);

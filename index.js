df4e5b082d20bd564afc72886b82112d2c435356
jamkerr <kerr0088@gmail.com>
2022-11-15 12:13:40 +0100

    Testing movies endpoint from client

--- index.js
+++ index.js
@@ -15,18 +15,20 @@ const Users = Models.User;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let allowedOrigins = '*';
+// let allowedOrigins = '*';
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+app.use(cors());
 
 let auth = require('./auth')(app);
 const passport = require('passport');

7a04b64a8302eaf1ceb371aeb495a4656f5808ec
jamkerr <kerr0088@gmail.com>
2023-02-08 10:29:27 +0100

    Add CORS origin for GH Pages hosted Angular app

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const Users = Models.User;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let allowedOrigins = ['http://localhost:1234', 'https://ymdb.netlify.app', 'http://localhost:4200'];
+let allowedOrigins = ['http://localhost:1234', 'https://ymdb.netlify.app', 'http://localhost:4200', 'https://jamkerr.github.io'];
 
 app.use(cors({
   origin: (origin, callback) => {
